afa335bb514f5917860071431a156691f1bc4625
JonneSaloranta <saloranta.jonne@gmail.com>
2024-10-18 11:50:17 +0300

    fix a comment

--- src/main/java/com/jonnesaloranta/enums/PlayerAnim.java
+++ src/main/java/com/jonnesaloranta/enums/PlayerAnim.java
@@ -5,7 +5,7 @@ public class PlayerAnim {
     public static final int ANIM_MINING = 7139;
     public static final int ANIM_WOODCUTTING = 2846;
     public static final int ANIM_WOODCUTTING_FELLING_AXE = 10071;
-    public static final int ANIM_FISHING = 7401; // dragon notification
+    public static final int ANIM_FISHING = 7401; // dragon harpoon
     public static final int ANIM_FISHING_1 = 618; // regular harpoon
     public static final int ANIM_FISHING_2 = 619; // lobster pot
     public static final int ANIM_FISHING_3 = 620; // big net
