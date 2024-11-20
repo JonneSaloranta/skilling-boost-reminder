87a54d87d7ec0bfa2f549420426409416dd4e6d6
Jonne <saloranta.jonne@gmail.com>
2024-11-20 20:29:07 +0200

    fix: added more animations for checking when player is doing any activity

--- src/main/java/com/jonnesaloranta/enums/PlayerAnim.java
+++ src/main/java/com/jonnesaloranta/enums/PlayerAnim.java
@@ -1,55 +1,89 @@
 package com.jonnesaloranta.enums;
 
+import net.runelite.api.AnimationID;
+
 public class PlayerAnim {
 
-    /**
-     * Mining with Dragon Pickaxe
-     * */
-    public static final int ANIM_MINING = 7139;
+    // MINING
+    public static final int ANIM_MINING1 = AnimationID.MINING_DRAGON_PICKAXE;
+    public static final int ANIM_MINING2 = AnimationID.MINING_DRAGON_PICKAXE_OR;
+    public static final int ANIM_MINING3 = AnimationID.MINING_DRAGON_PICKAXE_OR_TRAILBLAZER;
+    public static final int ANIM_MINING4 = AnimationID.MINING_DRAGON_PICKAXE_UPGRADED;
 
-    /**
-     * Woodcutting with Dragon Axe
-     * */
-    public static final int ANIM_WOODCUTTING = 2846;
+    public static final int ANIM_MINING5 = AnimationID.MINING_CRASHEDSTAR_DRAGON;
+    public static final int ANIM_MINING6 = AnimationID.MINING_CRASHEDSTAR_DRAGON_OR;
+    public static final int ANIM_MINING7 = AnimationID.MINING_CRASHEDSTAR_DRAGON_OR_TRAILBLAZER;
+    public static final int ANIM_MINING8 = AnimationID.MINING_CRASHEDSTAR_DRAGON_UPGRADED;
 
-    /**
-     * Woodcutting with Dragon Felling Axe
-     * */
-    public static final int ANIM_WOODCUTTING_FELLING_AXE = 10071;
+    public static final int ANIM_MINING9 = AnimationID.MINING_MOTHERLODE_DRAGON;
+    public static final int ANIM_MINING10 = AnimationID.MINING_MOTHERLODE_DRAGON_OR;
+    public static final int ANIM_MINING11 = AnimationID.MINING_MOTHERLODE_DRAGON_OR_TRAILBLAZER;
+    public static final int ANIM_MINING12 = AnimationID.MINING_MOTHERLODE_DRAGON_UPGRADED;
 
-    /**
-     * Fishing with Dragon Harpoon
-     * */
-    public static final int ANIM_FISHING = 7401;
+    public static final int ANIM_MINING13 = AnimationID.MINING_3A_PICKAXE;
+    public static final int ANIM_MINING14 = AnimationID.MINING_CRASHEDSTAR_3A;
+    public static final int ANIM_MINING15 = AnimationID.MINING_MOTHERLODE_3A;
 
-    /**
-     * Regular Harpoon
-     * */
-    public static final int ANIM_FISHING_1 = 618;
+    public static final int ANIM_MINING16 = AnimationID.MINING_INFERNAL_PICKAXE;
+    public static final int ANIM_MINING17 = AnimationID.MINING_CRASHEDSTAR_INFERNAL;
+    public static final int ANIM_MINING18 = AnimationID.MINING_MOTHERLODE_INFERNAL;
 
-    /**
-     * Lobster Pot
-     * */
-    public static final int ANIM_FISHING_2 = 619;
+    public static final int ANIM_MINING19 = AnimationID.MINING_CRYSTAL_PICKAXE;
+    public static final int ANIM_MINING20 = AnimationID.MINING_CRASHEDSTAR_CRYSTAL;
+    public static final int ANIM_MINING21 = AnimationID.MINING_MOTHERLODE_CRYSTAL;
 
-    /**
-     * Big Fishing net
-     * */
-    public static final int ANIM_FISHING_3 = 620;
+    // WOODCUTTING
+    public static final int ANIM_WOODCUTTING1 = AnimationID.WOODCUTTING_DRAGON;
+    public static final int ANIM_WOODCUTTING2 = AnimationID.WOODCUTTING_DRAGON_OR;
+    public static final int ANIM_WOODCUTTING3 = AnimationID.WOODCUTTING_2H_DRAGON;
 
-    /**
-     * Small Fishing Net
-     * */
-    public static final int ANIM_FISHING_4 = 621;
+    public static final int ANIM_WOODCUTTING4 = AnimationID.WOODCUTTING_ENT_DRAGON_OR;
+    public static final int ANIM_WOODCUTTING5 = AnimationID.WOODCUTTING_ENT_DRAGON;
+    public static final int ANIM_WOODCUTTING6 = AnimationID.WOODCUTTING_ENT_2H_DRAGON;
 
-    /**
-     * Fishing Rod (throw tackle)
-     * */
-    public static final int ANIM_FISHING_5 = 622;
+    public static final int ANIM_WOODCUTTING7 = AnimationID.WOODCUTTING_CRYSTAL;
+    public static final int ANIM_WOODCUTTING8 = AnimationID.WOODCUTTING_2H_CRYSTAL;
+    public static final int ANIM_WOODCUTTING9 = AnimationID.WOODCUTTING_ENT_CRYSTAL;
+    public static final int ANIM_WOODCUTTING10 = AnimationID.WOODCUTTING_ENT_2H_CRYSTAL;
+    // might not work
+    public static final int ANIM_WOODCUTTING11 = AnimationID.WOODCUTTING_2H_CRYSTAL_INACTIVE;
+    public static final int ANIM_WOODCUTTING12 = AnimationID.WOODCUTTING_ENT_CRYSTAL_INACTIVE;
+    public static final int ANIM_WOODCUTTING13 = AnimationID.WOODCUTTING_ENT_2H_CRYSTAL_INACTIVE;
 
-    /**
-     * Fishing Rod (fishing)
-     * */
-    public static final int ANIM_FISHING_6 = 623;
+
+    public static final int ANIM_WOODCUTTING14 = AnimationID.WOODCUTTING_INFERNAL;
+    public static final int ANIM_WOODCUTTING15 = AnimationID.WOODCUTTING_ENT_INFERNAL;
+    public static final int ANIM_WOODCUTTING16 = AnimationID.WOODCUTTING_ENT_INFERNAL_OR;
+
+    public static final int ANIM_WOODCUTTING17 = AnimationID.WOODCUTTING_3A_AXE;
+    public static final int ANIM_WOODCUTTING18 = AnimationID.WOODCUTTING_2H_3A;
+    public static final int ANIM_WOODCUTTING19 = AnimationID.WOODCUTTING_ENT_3A;
+    public static final int ANIM_WOODCUTTING20 = AnimationID.WOODCUTTING_ENT_2H_3A;
+
+    // FISHING
+    public static final int ANIM_FISHING1 = AnimationID.FISHING_DRAGON_HARPOON;
+    public static final int ANIM_FISHING2 = AnimationID.FISHING_DRAGON_HARPOON_OR;
+
+    public static final int ANIM_FISHING3 = AnimationID.FISHING_INFERNAL_HARPOON;
+    public static final int ANIM_FISHING4 = AnimationID.FISHING_CRYSTAL_HARPOON;
+    public static final int ANIM_FISHING5 = AnimationID.FISHING_TRAILBLAZER_HARPOON;
+
+    public static final int ANIM_FISHING6 = AnimationID.FISHING_HARPOON;
+    public static final int ANIM_FISHING7 = AnimationID.FISHING_CAGE;
+    public static final int ANIM_FISHING8 = AnimationID.FISHING_BIG_NET;
+    public static final int ANIM_FISHING9 = AnimationID.FISHING_NET;
+
+    public static final int ANIM_FISHING10 = AnimationID.FISHING_OILY_ROD;
+
+    public static final int ANIM_FISHING11 = AnimationID.FISHING_POLE_CAST;
+
+    public static final int ANIM_FISHING12 = AnimationID.FISHING_KARAMBWAN;
+
+    public static final int ANIM_FISHING13 = AnimationID.FISHING_PEARL_ROD;
+    public static final int ANIM_FISHING14 = AnimationID.FISHING_PEARL_FLY_ROD;
+    public static final int ANIM_FISHING15 = AnimationID.FISHING_PEARL_FLY_ROD_2;
+
+    public static final int ANIM_FISHING16 = AnimationID.FISHING_BARBARIAN_ROD;
+    public static final int ANIM_FISHING17 = AnimationID.FISHING_PEARL_BARBARIAN_ROD_2;
 
 }
